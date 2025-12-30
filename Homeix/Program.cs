07686304cfcd134ef60b8f320cc575429369a7d7
m7mdbarawi1 <m7mdbarawi@gmail.com>
2025-12-30 12:42:20 +0300

    fix translation and auth middlewares

--- Homeix/Program.cs
+++ Homeix/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Homeix.Data;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var cs = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string not found.");
-
 builder.Services.AddDbContext<HOMEIXDbContext>(opt => opt.UseSqlServer(cs));
 
 // ======================
@@ -13,18 +15,56 @@ builder.Services.AddDbContext<HOMEIXDbContext>(opt => opt.UseSqlServer(cs));
 // ======================
 builder.Services.AddAuthentication("HomeixAuth").AddCookie("HomeixAuth", options =>
     {
+        options.Cookie.Name = "Homeix.Auth";
         options.LoginPath = "/Account/Login";
         options.AccessDeniedPath = "/Account/AccessDenied";
-        options.ExpireTimeSpan = TimeSpan.FromHours(5);
         options.SlidingExpiration = true;
+        options.ExpireTimeSpan = TimeSpan.FromHours(5);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
     });
 
 builder.Services.AddAuthorization();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+// 3) Localization options
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] {
+        new CultureInfo("en-US"),
+        new CultureInfo("ar-JO")
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+    // ✅ allow switching via query string and cookie
+    options.RequestCultureProviders = new List<IRequestCultureProvider>
+    {
+        new QueryStringRequestCultureProvider(),
+        new CookieRequestCultureProvider()
+    };
+});
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+// Enable localization
+var locOptions = app.Services.GetRequiredService<Microsoft.Extensions.Options.IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(locOptions.Value);
+
+
+// 4) Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
