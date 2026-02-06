b020408b1dda77b080e389d1e1309e96cb6555cf
m7mdbarawi1 <m7mdbarawi@gmail.com>
2026-02-07 01:29:53 +0300

    add apk download

---
+++ Homeix/Controllers/DownloadsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+public class DownloadsController : Controller
+{
+    public IActionResult MobileApp()
+    {
+        // Find the file
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "downloads", "mobile.apk");
+
+        if (System.IO.File.Exists(filePath))
+        {
+            // Return the file for download
+            return PhysicalFile(filePath, "application/vnd.android.package-archive", "Homeix.apk");
+        }
+
+        return NotFound("App file not found");
+    }
+}
